8874837dad998bbb9a5a8485682bf61c9b8aca1f
Gregory SANCHEZ <gsanchez@chubchub.OREDIS-VP.local>
2018-12-07 17:18:30 +0100

    fix(deploy): fix username

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for tag in $tags
 do
-  ansible-container push --tag $tag --username wilmardo --password "$docker_passwd" --push-to docker
+  ansible-container push --tag $tag --username chubchubsancho --password "$docker_passwd" --push-to docker
 done
